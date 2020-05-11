1c5152f5cc9c98364185ae5c63199f80a8bf3bf1
RohitRajP <RohitRajP>
2020-05-12 01:25:40 +0530

    Fetched the audio title and artist for each type album, trending and artist

--- controllers/fetchGaana.js
+++ controllers/fetchGaana.js
@@ -2,6 +2,7 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 const puppeteer = require("puppeteer");
+const fs = require("fs");
 
 // gets the html content of the playlist page
 const getHTMLContent = async (playlistURL, res) => {
@@ -25,6 +26,69 @@ const getHTMLContent = async (playlistURL, res) => {
   }
 };
 
+// get song list through non-film album method
+const getSongLstNonFilmStructure = async ($) => {
+  // holds the list of song titles
+  const songsLst = [];
+  // fetching all Divs containing all songs
+  const songDivs = $(".content-container").find(".track_npqitemdetail");
+  // iterating through each div
+  songDivs.each((i, songDiv) => {
+    // constains instance of each song
+    let songObj = {};
+    // getting the song title
+    songObj["title"] = $(songDiv).find("span").text();
+    // cycling through the artists and getting the first one
+    $(songDiv)
+      .find("a")
+      .each(async (i, artistAnchor) => {
+        songObj["artist"] = await $(artistAnchor).text();
+      });
+    // checking if the audio artist is undefined, which would mean this is a movie album and thus, the artist name is in different
+    songsLst.push(songObj);
+  });
+  // returning songLst
+  return songsLst;
+};
+
+// get song list through film album method
+const getSongLstFilmStructure = async ($) => {
+  // holds the list of song titles
+  const songsLst = [];
+  // getting to the unordered list containing all songs
+  const allSongUL = await $(".content-container")
+    .find(".s_c")
+    .find("ul")
+    .toArray()[1];
+
+  // finding all the list items (songs in the unordered list)
+  const songsLstItems = await $(allSongUL)
+    .find('li[draggable="true"]')
+    .toArray();
+
+  // iterating through each song list item which contains multiple other list items
+  for (const multiListItems of songsLstItems) {
+    // constains instance of each song
+    let songObj = {};
+    // getting to the list item containing the song details
+    let songDetailLstItem = await $(multiListItems).find("li").toArray()[2];
+    // getting the song title stored in the 3rd list item
+    songObj["title"] = await $(
+      $(songDetailLstItem).find("a").toArray()[0]
+    ).text();
+    // getting the song artist stored in the 3rd list item
+    songObj["artist"] = await $(
+      $(songDetailLstItem).find("a").toArray()[1]
+    ).text();
+
+    // pushing song object into list
+    songsLst.push(songObj);
+  }
+
+  // returning the song lst
+  return songsLst;
+};
+
 // gets the audio song titles from the html document and album title
 const getAlbumInfo = async (htmlContent, res) => {
   try {
@@ -33,27 +97,17 @@ const getAlbumInfo = async (htmlContent, res) => {
     // fetching the album title
     let albumTitle = $("._d_tp_det").find("h1").text();
     // checking if albumTitle returned empty (if this is a trending list)
-    if (albumTitle.length === 0) {
-      albumTitle = $(".trendingtitle").text();
-    }
-    // holds the list of song titles
-    const songsLst = [];
-    // fetching all Divs containing all songs
-    const songDivs = $(".content-container").find(".track_npqitemdetail");
-    // iterating through each div
-    songDivs.each((i, songDiv) => {
-      // constains instance of each song
-      let songObj = {};
-      // getting the song title
-      songObj["title"] = $(songDiv).find("span").text();
-      // cycling through the artists and getting the first one
-      $(songDiv)
-        .find("a")
-        .each((i, artistAnchor) => {
-          songObj["artist"] = $(artistAnchor).text();
-        });
-      songsLst.push(songObj);
-    });
+    if (albumTitle.length === 0) albumTitle = $(".trendingtitle").text();
+    // checking if the albumTitle is still empty (if this is a movie album)
+    if (albumTitle.length === 0) albumTitle = $(".album_songheading").text();
+
+    // try getting the song list through non-film album method
+    let songsLst = await getSongLstNonFilmStructure($);
+
+    // checking if the artists have been fetched (if the album is a film album)
+    if (songsLst[0]["artist"] === undefined)
+      songsLst = await getSongLstFilmStructure($);
+
     // creating album object
     const albumObj = { albumTitle: albumTitle };
     // pushing album songs into albumObj
