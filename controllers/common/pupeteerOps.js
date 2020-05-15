159cb132857a420d26b5dce92d1ee4d2302dde7b
RohitRajP <RohitRajP>
2020-05-15 15:17:53 +0530

    Added useragent command

--- controllers/common/pupeteerOps.js
+++ controllers/common/pupeteerOps.js
@@ -8,9 +8,10 @@ module.exports.puppeteerOperation = async (playlistURL) => {
         const browser = await puppeteer.launch({
             args: ["--no-sandbox", "--disable-setuid-sandbox", "--proxy-server=http://14.140.131.82:3128"],
         });
-
         // navigate to the playlist page
         const page = await browser.newPage();
+        // set user agent (override the default headless User Agent)
+        await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
         // navigating to playlist page and waiting till the page loads
         await page.goto(playlistURL, {
             waitUntil: "networkidle2",
