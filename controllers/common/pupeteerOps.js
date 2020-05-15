4769e79d0f4781c06531423166a34da3c55a235c
RohitRajP <RohitRajP>
2020-05-15 14:50:34 +0530

    Added Proxy server

--- controllers/common/pupeteerOps.js
+++ controllers/common/pupeteerOps.js
@@ -6,13 +6,15 @@ module.exports.puppeteerOperation = async (playlistURL) => {
     try {
         // initializing puppeteer instance
         const browser = await puppeteer.launch({
-            args: ["--no-sandbox", "--disable-setuid-sandbox"],
+            args: ["--no-sandbox", "--disable-setuid-sandbox", "--proxy-server=http://14.140.131.82:3128"],
         });
+
         // navigate to the playlist page
         const page = await browser.newPage();
         // navigating to playlist page and waiting till the page loads
         await page.goto(playlistURL, {
-            waitUntil: "networkidle2"
+            waitUntil: "networkidle2",
+            timeout: 0
         });
         // getting html content of the page
         const html = await page.content();
